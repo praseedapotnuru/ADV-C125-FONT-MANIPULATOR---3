3248cc11c440d14f37dc1ae101d0ecfd877be01e
praseedapotnuru <82453459+praseedapotnuru@users.noreply.github.com>
2022-08-10 14:23:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
+
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
+
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
+}
+
+function draw(){
+    background("#5196e3");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#00ff0a");
+    text('Praseeda',50,400);
+}
+
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
+}
+
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
+}
